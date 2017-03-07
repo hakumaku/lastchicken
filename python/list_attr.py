cd97b249f3cc1de842bae9c2f848549a305af255
hakumaku <gentlebuuny@gmail.com>
2017-03-07 04:27:22 -0800

    updated

--- python/list_attr.py
+++ python/list_attr.py
@@ -1,5 +1,10 @@
 list_one = [12, 23, 'efgh', 'abcd']
 list_two = ['ab', 'cd', 'ef', 'gh', 'ij']
+list_nest=[
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9]
+]
 
 #
 # Changing an element in a list.
@@ -49,3 +54,26 @@ list_one.extend([1, 2, 3])
 print(list_one)
 list_one.insert(0, 'pqrst')
 print(list_one)
+
+#
+# Beware of reference and copy
+#
+temp = [1, 2, 3]
+list_one = ['a', temp, 'c']		# reference
+list_two = ['a', temp[:], 'c']	# copy
+print('\nShallow copy & deep copy')
+print(list_one, list_two)
+print('Is list_one[1] list_two[1]?? : {}'.format(list_one[1] is list_two[1]))
+temp[1] = 5						# affects list_one, but not list_two
+print(list_one, list_two)
+
+#
+# Can make a nested list
+# temp_nested = temp[:] makes sense, but not *Pythonic*.
+# Much better use list(temp)
+#
+temp_nested = list(temp)
+temp = temp * 3
+temp_nested = [temp_nested] * 3
+print('temp : {}'.format(temp))					# [a,b,c, ... ,a,b,c]
+print('temp_nested : {}'.format(temp_nested))	# [[a,b,c], ... , [a,b,c]]
