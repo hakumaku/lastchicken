6c3153cae60fac7a571c8238e4919bf02815cbae
hakumaku <gentlebuuny@gmail.com>
2017-11-13 13:19:43 +0900

    updated

--- C/system_programming/show_bytes.c
+++ C/system_programming/show_bytes.c
@@ -7,15 +7,24 @@ bool is_litend();
 int main(void)
 {
 	int x = 123;
+	printf("int x = 0x%.*x (address: %p)\n",
+			(int)(2*sizeof(int)), x, &x);
 	show_bytes(&x, sizeof(x));
-	printf("%d\n", is_litend());
+	if (is_litend())
+	{
+		puts("It is little-endian.");
+	}
+	else
+	{
+		puts("It is big-endian.");
+	}
 
 	return 0;
 }
 
 void show_bytes(void *ptr, size_t size)
 {
-	unsigned char *end = (unsigned char*)ptr + size;
+	unsigned char *end = (unsigned char *)ptr + size;
 	unsigned char *byte = (unsigned char *)ptr;
 	while (byte < end)
 	{
