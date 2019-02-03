9b94fdd4b63a6ada2ea93732b4cbc1ff548c6ad7
hakumaku <gentlebuuny@gmail.com>
2019-02-04 08:01:20 +0900

    updated

--- install/adapta-eta.sh
+++
@@ -1,3 +0,0 @@
-gsettings set org.gnome.desktop.interface gtk-theme 'Adapta-Eta'
-gsettings set org.gnome.shell.extensions.user-theme name 'Adapta-Eta'
-
--- install/arc-darker.sh
+++
@@ -1,3 +0,0 @@
-gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Darker'
-gsettings set org.gnome.shell.extensions.user-theme name 'Arc-Darker'
-
