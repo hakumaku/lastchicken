87d48e54bb36eaa1d61af2c969dfe705982a42b2
hakumaku <gentlebuuny@gmail.com>
2019-04-05 17:00:58 +0900

    updated

--- install/new/adapta.sh
+++ install/new/adapta.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 DIR="$HOME/workspace"
-OPTIONS="--enable-parallel --disable-cinnamon --disable-flashback"`
-	`"--disable-xfce --disable-mate --disable-openbox"`
-	`"--enable-gtk_next --with-selection_color --with-accent_color"`
-	`"--with-suggestion_color --with-destruction_color"
+OPTIONS=(
+	"--enable-parallel"
+	"--disable-cinnamon"
+	"--disable-flashback"
+	"--disable-xfce"
+	"--disable-mate"
+	"--disable-openbox"
+	"--enable-gtk_next"
+	"--with-selection_color"
+	"--with-accent_color"
+	"--with-suggestion_color"
+	"--with-destruction_color"
+)
 
 sudo rm -rf /usr/share/themes/{Adapta,Adapta-Eta,Adapta-Nokto,Adapta-Nokto-Eta}
 rm -rf ~/.local/share/themes/{Adapta,Adapta-Eta,Adapta-Nokto,Adapta-Nokto-Eta}
@@ -14,5 +23,5 @@ sudo apt -qq -y install autoconf automake inkscape libgdk-pixbuf2.0-dev \
 	libglib2.0-dev libxml2-utils pkg-config sassc parallel &&
 git clone -q https://github.com/adapta-project/adapta-gtk-theme \
 	"$DIR/adapta-gtk-theme" && (cd "$DIR/adapta-gtk-theme" &&
-	./autogen.sh --prefix=/usr $OPTIONS && make && sudo make install)
+	./autogen.sh --prefix=/usr "${OPTIONS[@]}" && make && sudo make install)
 
