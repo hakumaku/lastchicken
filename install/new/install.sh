f0bf48f3b827e6d0bbc4aabd80ea6f3ced61f7a9
hakumaku <gentlebuuny@gmail.com>
2019-04-05 11:42:42 +0900

    updated

--- install/new/install.sh
+++ install/new/install.sh
@@ -4,16 +4,31 @@ dir="$(dirname "$0")"
 while getopts "is" opt; do
 	case "$opt" in
 		"i")
+			bash "$dir""/apt.sh"
+			bash "$dir""/font.sh"
+			bash "$dir""/suckless.sh" -i st
+			bash "$dir""/suckless.sh" -i dmenu
+			bash "$dir""/powerline.sh" -i
+			bash "$dir""/ranger.sh" -i ranger
+			bash "$dir""/ranger.sh" -i ranger_devicons
+			bash "$dir""/sxiv.sh" -i
+
+			bash "$dir""/vim.sh" -i
+			bash "$dir""/tmux.sh" -i
+
+			bash "$dir""/suru.sh"
+			python3 "$dir""/gnome.py"
+			bash "$dir""/config.sh" -i
+			bash "$dir""/gsettings.sh"
 			break;;
 		"s")
-			source "$dir"/foo.sh
-			bash ./config.sh -s
-			# source "$dir""/powerline.sh" -s
-			# source "$dir""/ranger.sh" -s
-			# source "$dir""/suckless.sh" -s
-			# source "$dir""/sxiv.sh" -s
-			# source "$dir""/tmux.sh" -s
-			# source "$dir""/vim.sh" -s
+			bash "$dir""/config.sh" -s
+			bash "$dir""/powerline.sh" -s
+			bash "$dir""/ranger.sh" -s
+			bash "$dir""/suckless.sh" -s
+			bash "$dir""/sxiv.sh" -s
+			bash "$dir""/tmux.sh" -s
+			bash "$dir""/vim.sh" -s
 			break;;
 		*)
 			break;;
